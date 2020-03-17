48b3b40e0e6b107984f03409b9fa2f00ac58ea5c
Ian <ian.sajkowicz@uconn.edu>
2020-03-17 12:20:32 -0400

    Update exporter.php

--- upload/admin/model/extension/module/exporter.php
+++ upload/admin/model/extension/module/exporter.php
@@ -4,14 +4,14 @@ class ModelExtensionModuleExporter extends Model {
 	//runs a SQL command to get relevant data entries from the database 
 	function getResults()
 	{	
-		$query = $this->db->query("select oc_order.email, oc_order.firstname, oc_order.lastname, oc_order_product.name, oc_category_description.name as type
-		from oc_order
-		inner JOIN oc_order_product
-		on oc_order_product.order_id = oc_order.order_id
-		inner JOIN oc_product_to_category
-		on oc_product_to_category.product_id = oc_order_product.product_id
-		inner JOIN oc_category_description
-		on oc_category_description.category_id = oc_product_to_category.category_id");
+		$query = $this->db->query("SELECT " . DB_PREFIX . "order.email, " . DB_PREFIX . "order.firstname, " . DB_PREFIX . "order.lastname, " . DB_PREFIX . "order_product.name, " . DB_PREFIX . "category_description.name as type
+		FROM " . DB_PREFIX . "order" 
+		. " INNER JOIN " . DB_PREFIX . "order_product" .  
+		" ON " . DB_PREFIX . "order_product.order_id = " . DB_PREFIX . "order.order_id"
+		. " INNER JOIN " . DB_PREFIX . "product_to_category" .  
+		" ON " . DB_PREFIX . "product_to_category.product_id = " . DB_PREFIX . "order_product.product_id" 
+		. " INNER JOIN " . DB_PREFIX . "category_description" .
+		" ON " . DB_PREFIX . "category_description.category_id = " . DB_PREFIX . "product_to_category.category_id");
 		
 		return $query;
 	}
